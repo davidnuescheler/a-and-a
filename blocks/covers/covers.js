dfc0ae4695556723c4f7e1e69923c6bb26c4f4fb
David Nuescheler <david@nuescheler.ch>
2022-08-20 20:30:50 -0600

    chore: add links

--- blocks/covers/covers.js
+++ blocks/covers/covers.js
@@ -4,5 +4,10 @@ export default async function decorate(block) {
     [...row.children].forEach((cell, i) => {
       if (classes[i]) cell.classList.add(`covers-${classes[i]}`);
     });
+    const [year, month] = row.children[1].textContent.split('/').map((t) => t.trim());
+    const a = document.createElement('a');
+    a.href = `/issues/${year}-${month}.pdf`;
+    a.append(row.children[0].querySelector('picture'));
+    row.children[0].append(a);
   });
 }
